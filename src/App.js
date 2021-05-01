123890030361471660bf7d5c1bc67fae0cf8305d
BuzaAlx <buzakivi@gmail.com>
2021-05-01 23:52:00 +0300

    Init app,create assets folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./pages/main";
+// import Swiper core and required modules
+// import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+// import { Swiper, SwiperSlide } from "swiper/react";
+import "./App.scss";
+
+// import "swiper/swiper.scss";
+// import "swiper/components/pagination/pagination.scss";
+
+// install Swiper modules
+// SwiperCore.use([Pagination]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Main />
+      {/* <div className="slider-container"> */}
+      {/* <Swiper
+          slidesPerView={1}
+          onSwiper={(swiper) => console.log(swiper)}
+          pagination={{
+            clickable: true,
+            renderBullet: (index, className) => {
+              return (
+                '<span class="' + className + '">' + (index + 1) + "</span>"
+              );
+            },
+          }}
+          onSlideChange={() => console.log("slide change")}
         >
-          Learn React
-        </a>
-      </header>
+          <SwiperSlide>
+            <div className="box">1</div>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <div className="box">2</div>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <div className="box">3</div>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <div className="box">4</div>
+          </SwiperSlide>
+        </Swiper> */}
+      {/* </div> */}
     </div>
   );
 }
